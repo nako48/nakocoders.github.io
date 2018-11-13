e9bf81d5bae5a7152ebee15353845bc2e94e979a
Arvan Apriyana <40161320+nako48@users.noreply.github.com>
2018-11-13 10:10:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Responsive Menu
+$(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(".multi-level-responsive-menu ul.menu").slideToggle();
+});
+$("ul.menu >li >ul").siblings("a").prepend("<span class='arrow-bottom'></span>").addClass("slidedown");
+	if (document.documentElement.clientWidth < 769) {
+	jQuery("ul.menu >li >ul li ul").siblings("a").prepend("<span class='arrow-bottom'></span>").addClass("slidedown");
+    	jQuery(".slidedown").click(function(e) {
+	      	e.preventDefault();
+	      	jQuery(this).siblings("ul").slideToggle();
+	    });
+	}
+	else if(document.documentElement.clientWidth > 769){
+	jQuery("ul.menu >li >ul li ul").siblings("a").prepend("<span class='arrow-right'></span>");
+}	
